7047192fc295979f5f6df522f34fea1d61037067
mauro <mauropuravida@gmail.com>
2020-09-08 13:14:27 -0300

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,9 @@
-import org.mozilla.jss.CryptoManager;
-import org.mozilla.jss.NotInitializedException;
+
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		try {
-			CryptoManager cryptoManager = CryptoManager.getInstance();
-		} catch (NotInitializedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		//HostNameMitmManaget msds;
 	}
 
 }
